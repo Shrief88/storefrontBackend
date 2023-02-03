be2b5a9d4e0ac54347f87ae51f4af5509aa9c0a8
Shrief Essam <shriefessam1999@gmail.com>
2023-02-03 14:49:06 +0200

    add jasmine setup for testing

--- dist/database.js
+++ dist/database.js
@@ -1,23 +1,16 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 var pg_1 = require("pg");
 dotenv_1.default.config();
-var _a = process.env,
-  POSTGRES_HOST = _a.POSTGRES_HOST,
-  POSTGRES_DB = _a.POSTGRES_DB,
-  POSTGRES_USER = _a.POSTGRES_USER,
-  POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD;
+var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD;
 var clinet = new pg_1.Pool({
-  host: POSTGRES_HOST,
-  database: POSTGRES_DB,
-  user: POSTGRES_USER,
-  password: POSTGRES_PASSWORD,
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
 });
-console.log(clinet);
 exports.default = clinet;
